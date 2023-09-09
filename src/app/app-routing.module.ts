a19f3784fe5e300d9e76acfd29743ba9ad7210f8
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-09-09 18:08:36 +0200

    creazione components di base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ReservationComponent } from './components/reservation/reservation.component';
+import { HotelListComponent } from './components/hotel/hotel-list/hotel-list.component';
+import { HotelDetailsComponent } from './components/hotel/hotel-details/hotel-details.component';
+import { RoomDetailsComponent } from './components/room/room-details/room-details.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'reservation', component: ReservationComponent },
+  { path: 'hotels', component: HotelListComponent },
+  { path: 'hotel/:id', component: HotelDetailsComponent },
+  { path: 'room/:id', component: RoomDetailsComponent },
+  { path: 'user-profile', component: UserProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HotelComponent } from './components/hotel/hotel.component';
+import { RoomComponent } from './components/room/room.component';
+import { ReservationComponent } from './components/reservation/reservation.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { HotelDetailsComponent } from './components/hotel/hotel-details/hotel-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    NavbarComponent,
+    HotelComponent,
+    RoomComponent,
+    ReservationComponent,
+    UserProfileComponent,
+    HotelDetailsComponent
   ],
   imports: [
     BrowserModule,
